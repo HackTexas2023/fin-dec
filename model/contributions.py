52863bdd23d78866dde71219911683fdb7d22482
Haakon <Haakon@modernintelligence.ai>
2023-10-22 06:37:30 -0500

    clean up model

--- model/contributions.py
+++ model/contributions.py
@@ -27,16 +27,16 @@ def contributions(income:float, account:str, contribution_size: float, current_a
     
     return account_balance, total_taxes
 
-age=60        
-account_balance1, tax_balance1 = contributions(100000.0, "traditional_ira",400.0, 20, age, 4381.25)
-account_balance2, tax_balance2 = contributions(100000.0, "roth_ira",400.0, 20, age, 4381.25)
-account_balance3, tax_balance3 = contributions(100000.0, "roth_401k",1200.0, 20, age, 4381.25)
-account_balance4, tax_balance4 = contributions(100000.0, "traditional_401k",1200.0, 20, age, 4381.25)
-plt.title("temp")
-plt.plot([i for i in range(len(tax_balance1))], [a_i - b_i for a_i, b_i in zip(account_balance1, tax_balance1)],label = "traditional_ira 1", linestyle="-")
-plt.plot([i for i in range(len(tax_balance2))], [a_i - b_i for a_i, b_i in zip(account_balance2, tax_balance2)],label = "roth_ira", linestyle="-.")
-plt.plot([i for i in range(len(tax_balance3))], [a_i - b_i for a_i, b_i in zip(account_balance3, tax_balance3)],label = "roth_401k", linestyle="--")
-plt.plot([i for i in range(len(tax_balance4))], [a_i - b_i for a_i, b_i in zip(account_balance4, tax_balance4)],label = "traditional_401k")
-plt.legend() 
+# age=60        
+# account_balance1, tax_balance1 = contributions(100000.0, "traditional_ira",400.0, 20, age, 4381.25)
+# account_balance2, tax_balance2 = contributions(100000.0, "roth_ira",400.0, 20, age, 4381.25)
+# account_balance3, tax_balance3 = contributions(100000.0, "roth_401k",1200.0, 20, age, 4381.25)
+# account_balance4, tax_balance4 = contributions(100000.0, "traditional_401k",1200.0, 20, age, 4381.25)
+# plt.title("temp")
+# plt.plot([i for i in range(len(tax_balance1))], [a_i - b_i for a_i, b_i in zip(account_balance1, tax_balance1)],label = "traditional_ira 1", linestyle="-")
+# plt.plot([i for i in range(len(tax_balance2))], [a_i - b_i for a_i, b_i in zip(account_balance2, tax_balance2)],label = "roth_ira", linestyle="-.")
+# plt.plot([i for i in range(len(tax_balance3))], [a_i - b_i for a_i, b_i in zip(account_balance3, tax_balance3)],label = "roth_401k", linestyle="--")
+# plt.plot([i for i in range(len(tax_balance4))], [a_i - b_i for a_i, b_i in zip(account_balance4, tax_balance4)],label = "traditional_401k")
+# plt.legend() 
 
-plt.show()
+# plt.show()
