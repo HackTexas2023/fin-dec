d01c97794f0e2cb43814f3dda8e04b4e7fe6758f
Haakon <Haakon@modernintelligence.ai>
2023-10-21 22:22:51 -0500

    roth_ira graph

--- model/base_models.py
+++ model/base_models.py
@@ -1,5 +1,7 @@
+import matplotlib.pyplot as plt
+
 def federal_tax_amount(total_income: float, filing: int) -> float:
-    amt = 0
+
     baseline_taxes = [0.0, 1100.0, 4046.88, 11142.78, 20813.76, 15727.68, 121405.9]
     break_point = [0.0, 11000.0,44725.0, 95375.0,182100.0, 231250.0, 578125.0, float('inf')]
     rates = [0.0, .10, .12, .22, .24,.32, .35, .37]
@@ -8,26 +10,49 @@ def federal_tax_amount(total_income: float, filing: int) -> float:
         if(total_income < val):
             remaining_income = total_income - break_point[i-1]
             total_taxed = sum(baseline_taxes[0:i]) + (remaining_income*rates[i]) + (total_income * 0.0765)
-            return total_taxed/12.0
-            break
-            
+            return total_taxed/12.0            
     
     return 0.0
 
-def Roth_ira(contribution_size: float, years_till_retirement: int)->None:
-    money_amount = [0]
 
-    for i in range(20, years_till_retirement):
-        for i in range(12):
+def roth_ira(contribution_size: float, current_age: int, retirement_age: int, retirement_spending: float)->[float]:
+    money_amount = [0]
+    retirement_spending = max(retirement_spending, 4381.25)
+
+    for i in range(current_age, retirement_age):
+        for j in range(12):
             curr_money = (money_amount[-1] + contribution_size) * 1.0085
             money_amount.append(curr_money)
 
-    for i in range(60 - years_till_retirement):
-        for i in range(12):
-            cost_of_living = (contribution_size + (max((4381.25 - contribution_size),)) * 1.10 )
-            curr_money = money_amount[-1] - cost_of_living - federal_tax_amount(cost_of_living, 0)
+    years_contributing = retirement_age - current_age
+
+    for i in range(60 - max(retirement_age, 60)):
+        for j in range(12):
+            if i < years_contributing:
+                cost_of_living = (max((retirement_spending- contribution_size), 0) * 1.10) + contribution_size
+            else:
+                cost_of_living = retirement_spending * 1.10 
+
+            curr_money = money_amount[-1] - cost_of_living
             curr_money = curr_money*1.0085
             money_amount.append(curr_money)
+
+    for i in range(30):
+        for j in range(12):
+            curr_money = money_amount[-1] - retirement_spending
+            curr_money = curr_money* 1.0085
+            money_amount.append(curr_money)
+
+    return money_amount
+
+
+account_balance = roth_ira(500, 18, 60, 4381.25)
+plt.plot([i for i in range(len(account_balance))], account_balance)
+plt.show()
+
+
+
     
     
-print(federal_tax_amount(60000.0, 1))
+    
+    
